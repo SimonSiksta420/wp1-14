fb74a744ae6ff415f4a8ec9a5ccb9350074db447
SimonSiksta420 <simon.siksta@student.ossp.cz>
2020-05-19 09:52:33 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+$submit = filter_input(INPUT_POST, 'submit');
+var_dump($submit);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+if (isset($submit)) {
+    echo "formulář byl odeslán";
+} else {
+    echo "stránka byla načtena bez odeslaného formuláře";
+}
+?>
+
+ <form action="index.php" method="post">
+  <label for="email"> E-mail: </label>
+  <input type="email" name="email" id="email">
+
+   <h2> Pohlaví </h2>
+   <label for="gender-male"> Muž </label>
+   <input type="radio" name="gender" value="male" id="gender-male">
+
+   <label for="gender-female"> Žena </label>
+   <input type="radio" name="gender" value="female" id="gender-female">
+
+   <label for="gender-none"> Nespecifikováno </label>
+   <input type="radio" name="gender" value="none" id="gender-none">
+
+   <input type="submit" name="submit" value="Odeslat">
+</body>
+</html>
